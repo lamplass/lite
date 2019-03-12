79ff949fa387098afbf977fc30db057f87fe941f
skyvoin <skyvoin14@mail.ru>
2019-03-12 13:39:53 +0700

    plusminus

--- Program.cs
+++ Program.cs
@@ -8,3 +8,11 @@
                         break;
                     }
 
+                    case '-':
+                    {
+                        int x = a;
+                        int y = b;
+                        int z = x - y;
+                        Console.WriteLine(z);
+                        break;
+                    }
