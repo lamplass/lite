6441e38ae0023acd7d9ff20505e66cd2842b1d9b
jojokabin <egorushkanutslove@gmail.com>
2019-03-12 12:12:54 +0700

    plus

--- Program.cs
+++ Program.cs
@@ -1,32 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+                case '+':
+                    {
+                        int x = a;
+                        int y = b;
+                        int z = x + y;
+                        Console.WriteLine(z);
+                        break;
+                    }
 
-namespace laba2_1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Введите А: ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите B: ");
-            int b = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите действие:");
-           
-            ConsoleKeyInfo Key = Console.ReadKey();
-
-
-          
-            char selection = Key.KeyChar;
-            switch (selection)
-            {
-               
-                    
-            }
-            Console.ReadKey();
-        }
-    }
-}
