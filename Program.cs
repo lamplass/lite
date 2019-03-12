47ad370df1c869180c64497b4819e4389f705d91
POPEYE <123>
2019-03-12 12:15:53 +0700

    del

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2_1
+{
+
+              case '/':
+                    {
+                        int x = a;
+                        int y = b;
+
+                        int z = x / y;
+                        Console.WriteLine(z);
+                        break;
+                    }
