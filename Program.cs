79bfe9f09adcc9fc903722373e99acd2fe38dc64
POPEYE <123>
2019-03-12 13:22:35 +0700

    delmng

--- Program.cs
+++ Program.cs
@@ -7,3 +7,12 @@
                         Console.WriteLine(z);
                         break;
                     }
+
+              case '*':
+                   {
+                        int x = a;
+                        int y = b;
+                        int z = x * y;
+                        Console.WriteLine(z);
+                        break;
+                    }
