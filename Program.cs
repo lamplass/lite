58445868d87aca628bbd12d6cfb003286e14f358
Skyvoin <skyvoin14@mail.ru>
2019-03-12 13:14:58 +0700

    qwe

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿
-                case '-':
-                    {
-                        int x = a;
-                        int y = b;
-                        int z = x - y;
-                        Console.WriteLine(z);
-                        break;
-                    }
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace laba2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите А: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите действие:");
+
+            ConsoleKeyInfo Key = Console.ReadKey();
+
+
+
+            char selection = Key.KeyChar;
+            switch (selection)
+            {
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
